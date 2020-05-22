032925f11df5e28b0ab4822b279d527074e866b2
Nero Vanbiervliet <nero.vanbiervliet@gmail.com>
2020-05-22 18:42:34 +0200

    add bug example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import './index.css';
+import React, {useState} from "react"
+
+const NO_URL = 'NO URL';
+
+const getUrl = () => {
+  if ((typeof window) === 'undefined') {
+    return NO_URL;
+  }
+
+  return window.location.href;
+}
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [url, setUrl] = useState(getUrl())
+  const invalidClass = url === NO_URL ? 'invalid' : '';
+  return <div className={invalidClass}>{`URL = ${url}`}</div>
 }
